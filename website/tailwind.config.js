7a49499ea1ab0013482b2f3afac25020d9888769
Oshioke-Salaki <salaki1902@gmail.com>
2024-05-22 21:39:48 +0100

    feat: made website responsive

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
       sans: ["Droid Sans", "sans-serif"],
     },
     extend: {
+      screens: {
+        phones: "500px",
+        tab: "600px",
+        mids: "1000px",
+        desktop: "1270px",
+      },
       backgroundImage: {
         herobg: 'url("/src/assets/heroBg.svg")',
+        mobileHeroBg: 'url("/src/assets/mobileHeroBg.svg")',
         contributeBg: 'url("/src/assets/contributeBg.svg")',
         footerBg: 'url("/src/assets/footerBg.svg")',
         gradientBg: 'url("/src/assets/gradientBg.svg")',
