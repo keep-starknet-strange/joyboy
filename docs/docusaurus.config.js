17deadc0d8b82c5a10624d9a8d9e7ff8467cf64c
calebux <petersoncaleb275@gmail.com>
2024-05-29 17:37:17 +0100

    minor fixes

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "joyboy", // Usually your GitHub org/user name.
+  projectName: "Joyboy", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
