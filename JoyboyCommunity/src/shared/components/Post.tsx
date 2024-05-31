9be528b2e42cd1396a5062eee086dd3ebcf26b18
bhavyagosai <bhavyagosai4@gmail.com>
2024-05-31 07:07:19 +0530

    feat: post card ui overhaul
    - updated ui + icons according to figma designs
    - updated reposted message with icon + better ui
    - made timestamps dynamic and readable
    - added post like feature + animation

--- JoyboyCommunity/src/shared/components/Post.tsx
+++ JoyboyCommunity/src/shared/components/Post.tsx
@@ -1,35 +1,126 @@
-import {MaterialIcons, Octicons} from '@expo/vector-icons';
+import {MaterialIcons} from '@expo/vector-icons';
 import {NDKEvent} from '@nostr-dev-kit/ndk';
 import {useNavigation} from '@react-navigation/native';
 import {Image, Pressable, Text, View} from 'react-native';
 import styled from 'styled-components/native';
 
 import {Typography} from '../../components';
+import {CommentIcon, LikeFillIcon, LikeIcon, RepostIcon} from '../../assets/icons';
+import {timestampToHumanReadable} from '../../utils/common-utils';
 import {MainStackNavigationProps, Post as PostType} from '../../types';
+import Animated, {
+  Easing,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withSequence,
+  withSpring,
+} from 'react-native-reanimated';
+import {useEffect, useState} from 'react';
+
+export const Icon = styled(View)`
+  padding-horizontal: 4px;
+`;
+
+const FlexView = styled(View)`
+  display: flex;
+  flex-direction: row;
+`;
+
+const RepostMessage = styled(Typography)`
+  font-size: 14px;
+  color: #8f979d;
+  position: relative;
+  bottom: 3px;
+  margin-bottom: 5px;
+`;
 
 const PostCard = styled(View)`
   background-color: #ffffff;
   border-radius: 16px;
-  padding: 8px;
+  padding: 10px;
   margin: 0 20px 18px 20px;
 `;
-const PostLayout = styled(View)`
-  flex-direction: row;
-`;
 
-export const InteractionContainer = styled(View)`
-  margin-top: 30px;
-  width: 100%;
+const PostHeader = styled(View)`
   display: flex;
-  gap: 8px;
   flex-direction: row;
-  align-content: center;
-  align-self: center;
   justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
 `;
 
-export const Icon = styled(View)`
-  padding-horizontal: 4px;
+const PostTitle = styled(Text)`
+  color: #121212;
+  font-weight: 500;
+  font-size: 17px;
+  letter-spacing: 0.5px;
+`;
+
+const PostSubtitleWrapper = styled(View)`
+  display: flex;
+  flex-direction: row;
+  margin-top: 4px;
+  align-items: center;
+`;
+
+const PostSubtitle = styled(Text)`
+  color: #8f979d;
+  font-size: 11px;
+  letter-spacing: 0.5px;
+`;
+
+const PostSubtitleCenterElement = styled(View)`
+  margin-horizontal: 8px;
+  height: 3px;
+  width: 3px;
+  border-radius: 1.5px;
+  background-color: #8f979d;
+`;
+
+const PostContent = styled(View)`
+  display: flex;
+  margin-bottom: 16px;
+`;
+
+const LikeWrapper = styled(View)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 6px;
+`;
+
+const LikeText = styled(Animated.Text)`
+  font-size: 14px;
+  color: #406686;
+  letter-spacing: 0.5px;
+`;
+
+const AnimatedIcon = styled(Animated.View)`
+  height: 26px;
+`;
+
+const PostFooter = styled(View)`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  gap: 8px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CommentWrapper = styled(View)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 6px;
+`;
+
+const CommentsText = styled(Text)`
+  color: #6b6b8c;
+  font-weight: 400;
+  font-size: 12px;
+  letter-spacing: 0.5px;
 `;
 
 interface PostProps {
@@ -42,6 +133,24 @@ interface PostProps {
 export const Post: React.FC<PostProps> = (props) => {
   const {post, event, repostedEvent} = props;
   const navigation = useNavigation<MainStackNavigationProps>();
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [likes, setLikes] = useState(2);
+
+  const scale = useSharedValue(1);
+  const textOpacity = useSharedValue(likes > 0 ? 1 : 0);
+  const iconPosition = useSharedValue(likes > 0 ? 0 : 6); // Adjusted to account for the gap
+
+  const animatedIconStyle = useAnimatedStyle(() => ({
+    transform: [{scale: withTiming(scale.value, {duration: 25, easing: Easing.out(Easing.ease)})}],
+    marginLeft: withTiming(iconPosition.value, {duration: 25}), // Animation for moving the icon
+  }));
+
+  const animatedTextStyle = useAnimatedStyle(() => ({
+    opacity: withTiming(textOpacity.value, {duration: 25}),
+    transform: [{scale: withTiming(textOpacity.value, {duration: 25})}],
+  }));
+
   const handleProfilePress = (userId?: string) => {
     if (userId) {
       navigation.navigate('Profile', {publicKey: userId});
@@ -51,9 +160,6 @@ export const Post: React.FC<PostProps> = (props) => {
   /** @TODO comment in Nostr */
   const handleComment = () => {};
 
-  /** @TODO comment in Nostr */
-  const handleLike = () => {};
-
   /** @TODO repost in Nostr */
   const handleRepostNote = () => {
     alert('Handle repost');
@@ -62,84 +168,111 @@ export const Post: React.FC<PostProps> = (props) => {
   const handleNavigateToPostDetails = () => {
     navigation.navigate('PostDetail', {postId: event?.id, post: event});
   };
+
   /** @TODO react in Nostr */
   const handleReact = () => {};
 
+  /** @TODO comment in Nostr */
+  const toggleLike = () => {
+    setIsLiked((prev) => !prev);
+    setLikes((prev) => {
+      const newLikes = isLiked ? prev - 1 : prev + 1;
+      textOpacity.value = newLikes > 0 ? 1 : 0;
+      iconPosition.value = newLikes > 0 ? 0 : 6;
+      return newLikes;
+    });
+    scale.value = withSequence(
+      withTiming(1.5, {duration: 25, easing: Easing.out(Easing.ease)}),
+      withSpring(1, {damping: 6, stiffness: 200}), // Small rebound effect
+    );
+  };
+
+  useEffect(() => {
+    if (likes > 0) setIsLiked(true);
+  }, []);
+
   return (
     <PostCard>
+      {/* to show reposted message */}
       {repostedEvent && (
-        <View>
-          <Typography>Reposted</Typography>
-        </View>
+        <FlexView>
+          <RepostIcon color="#8f979d" height={18} />
+          <RepostMessage>Reposted</RepostMessage>
+        </FlexView>
       )}
       {/* TODO different rendering base on kind =1,6,7 and tags for kind = 1 */}
-      <PostLayout>
-        <View style={{marginRight: 10}}>
+      <PostHeader>
+        <FlexView>
           <Pressable onPress={() => handleProfilePress(event?.pubkey)}>
             <Image
               source={props?.sourceUser ?? require('../../../assets/joyboy-logo.png')}
-              style={{width: 44, height: 44}}
+              style={{width: 50, height: 50}}
             />
           </Pressable>
-        </View>
 
-        <View style={{gap: 4, flex: 1}}>
-          <Pressable onPress={handleNavigateToPostDetails}>
-            <Text style={{color: 'black', fontWeight: '700'}}>{event?.pubkey}</Text>
-
-            {post?.source && (
-              <Image
-                source={{uri: post.source}}
-                style={{
-                  width: '100%',
-
-                  height: 200,
-                  borderRadius: 8,
-                  marginTop: 8,
-                }}
-              />
-            )}
+          <Pressable onPress={handleNavigateToPostDetails} style={{marginLeft: 10}}>
+            {/* using static data for name */}
+            <PostTitle>Monkey D Luffy</PostTitle>
+            <PostSubtitleWrapper>
+              <PostSubtitle>@luffy</PostSubtitle>
+              <PostSubtitleCenterElement />
+              <PostSubtitle>{timestampToHumanReadable(event.created_at)}</PostSubtitle>
+            </PostSubtitleWrapper>
           </Pressable>
-        </View>
+        </FlexView>
 
-        {/* TODO check tags if it's:
+        <Pressable onPress={toggleLike}>
+          <LikeWrapper>
+            <AnimatedIcon style={animatedIconStyle}>
+              {isLiked ? <LikeFillIcon height={26} /> : <LikeIcon height={26} />}
+            </AnimatedIcon>
+            {likes > 0 && (
+              <LikeText style={animatedTextStyle}>
+                {likes} {likes === 1 ? 'like' : 'likes'}
+              </LikeText>
+            )}
+          </LikeWrapper>
+        </Pressable>
+      </PostHeader>
+
+      <PostContent>
+        <Pressable onPress={handleNavigateToPostDetails}>
+          <Text>{repostedEvent?.content ? repostedEvent?.content : event?.content}</Text>
+
+          {post?.source && (
+            <Image
+              source={{uri: post.source}}
+              style={{
+                width: '100%',
+                height: 160,
+                borderRadius: 8,
+                marginTop: 14,
+                objectFit: 'cover',
+              }}
+            />
+          )}
+        </Pressable>
+      </PostContent>
+
+      {/* TODO check tags if it's:
         quote
         repost
         reply  */}
-        <Icon
-          as={Octicons}
-          name="heart"
-          size={24}
-          color="black"
-          onPress={handleLike}
-          style={{alignSelf: 'center'}}
-        />
-      </PostLayout>
-      <Pressable onPress={handleNavigateToPostDetails}>
-        <Text style={{color: 'black', marginTop: 10}}>
-          {repostedEvent?.content ? repostedEvent?.content : event?.content}
-        </Text>
-      </Pressable>
-      <InteractionContainer>
-        <Pressable onPress={handleNavigateToPostDetails} style={{flex: 1}}>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}
-          >
-            <Icon as={Octicons} name="comment" size={18} color="#406686" onPress={handleComment} />
-            <Text style={{color: '#406686', fontWeight: '500', fontSize: 11}}>16 comments</Text>
-          </View>
+      <PostFooter>
+        <Pressable onPress={handleNavigateToPostDetails}>
+          <CommentWrapper>
+            <CommentIcon height={26} onPress={handleComment} />
+            <CommentsText>16 comments</CommentsText>
+          </CommentWrapper>
         </Pressable>
         <Icon
           as={MaterialIcons}
           name="more-horiz"
-          size={18}
-          color="#406686"
+          size={26}
+          color="#6b6b8c"
           onPress={handleReact}
         />
-      </InteractionContainer>
+      </PostFooter>
     </PostCard>
   );
 };
