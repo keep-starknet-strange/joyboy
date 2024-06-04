483387524fefe64012a23102d8d39a1e466a5263
ickah <barukab68@gmail.com>
2024-06-04 13:20:40 +0200

    add function show answer

--- website/src/components/FaqBar.tsx
+++ website/src/components/FaqBar.tsx
@@ -13,15 +13,18 @@ const FaqBar: React.FC<Props> = ({ question, answer }) => {
   };
   return (
     <>
-      <button onClick={handleClick}>
-        <div className="py-4 tab:py-[32px] px-3 tab:px-[25px] flex justify-between items-center text-white border-[1px] border-[#C9C9C9] bg-black rounded-xl max-w-[871px] desktop:w-[871px] w-full">
+      <button
+        onClick={handleClick}
+        className=" py-4 tab:py-[32px] px-3 tab:px-[25px] border-[1px] border-[#C9C9C9]  text-white  bg-black rounded-xl max-w-[871px] desktop:w-[871px] w-full"
+      >
+        <div className=" flex justify-between items-center">
           <h2 className="desktop:text-[20px] text-xs leading-[28px]">
             {question}
           </h2>
           <img src={downCheveron} alt="" />
         </div>
 
-        <div>{isOpen ? <p>{answer}</p> : ""}</div>
+        <div>{isOpen ? <p className=" w-full text-start mt-6">{answer}</p> : ""}</div>
       </button>
     </>
   );
