978d7143b5a69d8a6e3227b1af4c0416d10621c4
ickah <barukab68@gmail.com>
2024-06-04 13:16:38 +0200

    add function show answer

--- website/src/components/FaqBar.tsx
+++ website/src/components/FaqBar.tsx
@@ -4,11 +4,26 @@ type Props = { answer: string; question: string };
 
 const FaqBar: React.FC<Props> = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    if (isOpen === true) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  };
   return (
-    <div className="py-4 tab:py-[32px] px-3 tab:px-[25px] flex justify-between items-center text-white border-[1px] border-[#C9C9C9] bg-black rounded-xl max-w-[871px] desktop:w-[871px] w-full">
-      <h2 className="desktop:text-[20px] text-xs leading-[28px]">{question}</h2>
-      <img src={downCheveron} alt="" />
-    </div>
+    <>
+      <button onClick={handleClick}>
+        <div className="py-4 tab:py-[32px] px-3 tab:px-[25px] flex justify-between items-center text-white border-[1px] border-[#C9C9C9] bg-black rounded-xl max-w-[871px] desktop:w-[871px] w-full">
+          <h2 className="desktop:text-[20px] text-xs leading-[28px]">
+            {question}
+          </h2>
+          <img src={downCheveron} alt="" />
+        </div>
+
+        <div>{isOpen ? <p>{answer}</p> : ""}</div>
+      </button>
+    </>
   );
 };
 
