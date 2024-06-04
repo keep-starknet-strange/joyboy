e225ffec4a7ee7531b3d79281f30a555bfd82585
Ben Ickah <89651828+benbaruka@users.noreply.github.com>
2024-06-04 14:51:15 +0200

    Fix UI dropdown action bug on Frequently asked Questions (#156)
    * add function show answer
    
    * add function show answer
    
    * fix dropdown action bug

--- website/src/components/FaqBar.tsx
+++ website/src/components/FaqBar.tsx
@@ -4,11 +4,29 @@ type Props = { answer: string; question: string };
 
 const FaqBar: React.FC<Props> = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    if (isOpen === true) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  };
   return (
-    <div className="py-4 tab:py-[32px] px-3 tab:px-[25px] flex justify-between items-center text-white border-[1px] border-[#C9C9C9] bg-black rounded-xl max-w-[871px] desktop:w-[871px] w-full">
-      <h2 className="desktop:text-[20px] text-xs leading-[28px]">{question}</h2>
-      <img src={downCheveron} alt="" />
-    </div>
+    <>
+      <button
+        onClick={handleClick}
+        className=" py-4 tab:py-[32px] px-3 tab:px-[25px] border-[1px] border-[#C9C9C9]  text-white  bg-black rounded-xl max-w-[871px] desktop:w-[871px] w-full"
+      >
+        <div className=" flex justify-between items-center">
+          <h2 className="desktop:text-[20px] text-xs leading-[28px]">
+            {question}
+          </h2>
+          <img src={downCheveron} alt="" />
+        </div>
+
+        <div>{isOpen ? <p className=" w-full text-start mt-6">{answer}</p> : ""}</div>
+      </button>
+    </>
   );
 };
 
