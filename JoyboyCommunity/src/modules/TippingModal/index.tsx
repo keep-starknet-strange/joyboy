50f896050848dd82d5d42b1025c6db65085ea05c
Uğur Eren <contact@ugureren.net>
2024-06-17 16:54:22 +0300

    Refactor tipping modal

--- JoyboyCommunity/src/modules/TippingModal/index.tsx
+++ JoyboyCommunity/src/modules/TippingModal/index.tsx
@@ -1,11 +1,8 @@
-import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {Image, View} from 'react-native';
-import {Portal} from 'react-native-portalize';
 
+import {Button, Modal, Text} from '../../components';
 import {useStyles} from '../../hooks';
-import {MainStackNavigationProps} from '../../types';
-import {Button, Text} from '../../components';
 import stylesheet from './styles';
 
 export type TippingModalProps = {
@@ -23,47 +20,35 @@ export const TippingModal: React.FC<TippingModalProps> = ({
 }) => {
   const styles = useStyles(stylesheet);
 
-  const navigation = useNavigation<MainStackNavigationProps>();
-
   if (!visible) return null;
 
   return (
-    <Portal>
-      <View style={styles.container}>
-        <View style={styles.modalContainer}>
-          <View style={styles.modal}>
-            <View style={styles.logo}>
-              <Image
-                style={styles.logoImage}
-                source={require('../../assets/tipping-modal.png')}
-                resizeMode="cover"
-              />
-            </View>
-
-            <View style={styles.content}>
-              <Text color="text" weight="bold" fontSize={21} lineHeight={24}>
-                Tipped {user}
-              </Text>
-              <Text color="primary" weight="bold" fontSize={21} lineHeight={24}>
-                {amount} {symbol}
-              </Text>
-              <Text color="textSecondary" weight="medium" fontSize={15} lineHeight={24}>
-                Keep spreading love
-              </Text>
-
-              <Button
-                style={styles.button}
-                onPress={() => {
-                  navigation.navigate('Home');
-                }}
-                variant="secondary"
-              >
-                Home
-              </Button>
-            </View>
-          </View>
+    <Modal>
+      <View style={styles.logoContainer}>
+        <View style={styles.logo}>
+          <Image
+            style={styles.logoImage}
+            source={require('../../assets/tipping-modal.png')}
+            resizeMode="cover"
+          />
         </View>
       </View>
-    </Portal>
+
+      <View style={styles.content}>
+        <Text color="text" weight="bold" fontSize={21} lineHeight={24}>
+          Tipped {user}
+        </Text>
+        <Text color="primary" weight="bold" fontSize={21} lineHeight={24}>
+          {amount} {symbol}
+        </Text>
+        <Text color="textSecondary" weight="medium" fontSize={15} lineHeight={24}>
+          Keep spreading love
+        </Text>
+      </View>
+
+      <Button block variant="secondary">
+        Continue
+      </Button>
+    </Modal>
   );
 };
--- JoyboyCommunity/src/modules/TippingModal/styles.ts
+++ JoyboyCommunity/src/modules/TippingModal/styles.ts
@@ -1,47 +1,28 @@
 import {Spacing, ThemedStyleSheet} from '../../styles';
 
+export const LOGO_SIZE = 250;
+
 export default ThemedStyleSheet((theme) => ({
-  container: {
-    flex: 1,
-  },
-  modalContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  logoContainer: {
     width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  modal: {
-    width: '80%',
-    height: '40%',
-    backgroundColor: theme.colors.surface,
-    borderRadius: 24,
+    height: LOGO_SIZE / 3,
   },
   logo: {
     position: 'absolute',
-    top: '-43%',
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    top: -(LOGO_SIZE / 2),
+    left: '50%',
+    width: LOGO_SIZE,
+    height: LOGO_SIZE,
   },
   logoImage: {
-    width: 238,
+    width: LOGO_SIZE,
+    height: LOGO_SIZE,
+    left: '-50%',
   },
   content: {
     gap: Spacing.small,
-    width: '100%',
-    height: '100%',
     alignItems: 'center',
     justifyContent: 'flex-end',
-    paddingHorizontal: Spacing.large,
-    paddingVertical: Spacing.medium,
-  },
-  button: {
-    width: '100%',
+    marginBottom: Spacing.medium,
   },
 }));
