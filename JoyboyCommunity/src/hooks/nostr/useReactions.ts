d6ba0fd518b5afd123f0c3a9d87bfacfaac078d6
Uğur Eren <contact@ugureren.net>
2024-06-27 20:14:15 +0300

    NIP-25 Integration

--- JoyboyCommunity/src/hooks/nostr/useReactions.ts
+++ JoyboyCommunity/src/hooks/nostr/useReactions.ts
@@ -1,38 +1,30 @@
 import {NDKKind} from '@nostr-dev-kit/ndk';
-import {useInfiniteQuery} from '@tanstack/react-query';
+import {useQuery} from '@tanstack/react-query';
 
 import {useNostrContext} from '../../context/NostrContext';
 
 export type UseReactionsOptions = {
   authors?: string[];
   search?: string;
+  noteId?: string;
 };
 
 export const useReactions = (options?: UseReactionsOptions) => {
   const {ndk} = useNostrContext();
 
-  return useInfiniteQuery({
-    initialPageParam: 0,
-    queryKey: ['reactions', options?.authors, options?.search],
-    getNextPageParam: (lastPage: any, allPages, lastPageParam) => {
-      if (!lastPage?.length) return undefined;
-
-      const pageParam = lastPage[lastPage.length - 1].created_at;
-
-      if (!pageParam || pageParam === lastPageParam) return undefined;
-      return pageParam;
-    },
-    queryFn: async ({pageParam}) => {
+  return useQuery({
+    queryKey: ['reactions', options?.authors, options?.search, options?.noteId],
+    queryFn: async () => {
       const notes = await ndk.fetchEvents({
         kinds: [NDKKind.Reaction],
         authors: options?.authors,
         search: options?.search,
-        until: pageParam || Math.round(Date.now() / 1000),
-        limit: 20,
+
+        '#e': options?.noteId ? [options.noteId] : undefined,
       });
 
       return [...notes];
     },
-    placeholderData: {pages: [], pageParams: []},
+    placeholderData: [],
   });
 };
