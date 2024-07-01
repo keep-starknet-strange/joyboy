88715bb37b86da7752f0970c450708edbbd9723d
Uğur Eren <contact@ugureren.net>
2024-07-01 16:08:45 +0300

    Remove backend package

--- backend/src/services/provider.ts
+++
@@ -1,7 +0,0 @@
-import {RpcProvider} from 'starknet';
-
-if (!process.env.PROVIDER_URL) throw new Error('PROVIDER_URL is not set');
-
-export const provider = new RpcProvider({
-  nodeUrl: process.env.PROVIDER_URL,
-});
