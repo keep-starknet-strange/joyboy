b97268fe976c53d7ed0acc496814184f09a128b4
Uğur Eren <contact@ugureren.net>
2024-07-15 13:06:56 +0300

    Change STRK to USDC

--- JoyboyCommunity/src/constants/tokens.ts
+++ JoyboyCommunity/src/constants/tokens.ts
@@ -11,7 +11,7 @@ export type MultiChainTokens = Record<TokenSymbol, MultiChainToken>;
 
 export enum TokenSymbol {
   ETH = 'ETH',
-  STRK = 'STRK',
+  USDC = 'USDC',
 }
 
 export const ETH: MultiChainToken = {
@@ -33,28 +33,28 @@ export const ETH: MultiChainToken = {
   },
 };
 
-export const STRK: MultiChainToken = {
+export const USDC: MultiChainToken = {
   [constants.StarknetChainId.SN_MAIN]: {
-    name: 'Stark',
-    symbol: TokenSymbol.STRK,
-    decimals: 18,
+    name: 'USD Coin',
+    symbol: TokenSymbol.USDC,
+    decimals: 6,
     address: getChecksumAddress(
-      '0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d',
+      '0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
     ),
   },
   [constants.StarknetChainId.SN_SEPOLIA]: {
-    name: 'Stark',
-    symbol: TokenSymbol.STRK,
-    decimals: 18,
+    name: 'USD Coin',
+    symbol: TokenSymbol.USDC,
+    decimals: 6,
     address: getChecksumAddress(
-      '0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d',
+      '0x053b40a647cedfca6ca84f542a0fe36736031905a9639a7f19a3c1e66bfd5080',
     ),
   },
 };
 
 export const TOKENS: MultiChainTokens = {
   [TokenSymbol.ETH]: ETH,
-  [TokenSymbol.STRK]: STRK,
+  [TokenSymbol.USDC]: USDC,
 };
 
 export const TOKEN_ADDRESSES: Record<
