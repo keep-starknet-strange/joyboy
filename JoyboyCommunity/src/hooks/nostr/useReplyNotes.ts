e31f2679ab21479e1372331c18848b4bbb8677cc
Uğur Eren <contact@ugureren.net>
2024-07-23 06:27:07 +0300

    Fix: follow & like & comment issues (#246)
    * fix: follow & like & comment
    
    * fix: follow created_at

--- JoyboyCommunity/src/hooks/nostr/useReplyNotes.ts
+++ JoyboyCommunity/src/hooks/nostr/useReplyNotes.ts
@@ -14,7 +14,7 @@ export const useReplyNotes = (options?: UseReplyNotesOptions) => {
 
   return useInfiniteQuery({
     initialPageParam: 0,
-    queryKey: ['replyNotes', ndk, options?.noteId, options?.authors, options?.search],
+    queryKey: ['replyNotes', options?.noteId, options?.authors, options?.search, ndk],
     getNextPageParam: (lastPage: any, allPages, lastPageParam) => {
       if (!lastPage?.length) return undefined;
 
